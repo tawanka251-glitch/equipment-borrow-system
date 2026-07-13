260f8208c9c450892bd6d9e464aa15a72a90734d
tawan seangkaew <tawanka251@gmail.com>
2026-07-13 14:45:18 +0700

    Update Docker port configuration

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3004;
 app.listen(PORT, async () => {
     try {
         await db.query('SELECT 1');
